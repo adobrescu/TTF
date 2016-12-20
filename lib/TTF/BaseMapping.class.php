4207d4d387459917817685b7ebb799cf913d3b58
Adi Dobrescu <adobrescu@insolitservices.ro>
2016-12-20 11:59:12 +0200

    Quick blindly adds static vars used for mapping

--- lib/TTF/BaseMapping.class.php
+++ lib/TTF/BaseMapping.class.php
@@ -5,6 +5,30 @@ namespace TTF;
 
 class BaseMapping
 {
+	/**
+	 *
+	 * @var array $___map - matrix used to map user input (provided conditions)
+	 *				to calculation formulas 
+	 */
+	static protected $___map=array(
+		array(true, true, false),
+		array(true, true, true),
+		array(false, true, true)
+	);
+	/**
+	 *
+	 * @var array $___X used to find "X", conditions -> $___map entry index -> $X
+	 */
+	static protected $___X=array('S', 'R', 'T');
+	/**
+	 *
+	 * @var array - conditions ->  $___map entry index -> $___formulas index (used with eval)
+	 */
+	static protected $___formulas=array(
+			'$params[0] + ($params[0] * $params[1] / 100)',
+			'$params[0] + ($params[0] * ($params[1] - $params[2]) / 100)',
+			'$params[0] - ($params[0] * $params[2] / 100)'
+		);
 	/*
 	 * As the class will work via a web service, it might be possible
 	 * to pass the input directly to the constructor and imediatly do the calculations
@@ -14,7 +38,12 @@ class BaseMapping
 	public function __construct()
 	{
 	}
-	public function map()
+	/**
+	 * 
+	 * @param type $conditionsList - list of booleans based on which the calcs are done
+	 * @param type $params - list of params involved in the calcs
+	 */
+	public function map($conditionsList, $params)
 	{
 	}
 }

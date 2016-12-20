16469c7df7d23293803804b6a60e798041004e93
Adi Dobrescu <adobrescu@insolitservices.ro>
2016-12-20 15:38:12 +0200

    Builds the "web service"

--- webroot/web-service.php
+++ webroot/web-service.php
@@ -1,10 +1,31 @@
 <?php
 
 include_once(__DIR__.'/../lib/WebService.class.php');
+include_once(__DIR__.'/../lib/TTF/MappingFactory.class.php');
 
 $service=new WebService();
 
-$service->appendResponse(array('status' => true, 'message' => 'This is a test', array('X'=>'S', 'Y'=>7)));
+$mp=new \TTF\MappingFactory();
+
+$mo=$mp->createMappingObject($_POST['service']);
+
+try
+{
+	//@todo validations
+	$ret=$mo->map($_POST['conditions'], $_POST['param']);
+	$service->appendResponse(array('status' => true, 'message' => 'Calculated values are: X='.$ret['X'].'; Y='.$ret['Y'], 'xy' => array('X'=>$ret['X'], 'Y'=>$ret['Y'])));
+}
+catch(\TTF\Exception $err)
+{
+	$service->appendResponse(array('status' => false, 'message' => $err->getMessage()));
+}
+catch(Exception $err)
+{
+	
+}
+
+
+
 
 $service->send();
 

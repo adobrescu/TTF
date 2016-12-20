82f49e33322a1d8ff86c84f79acba7ec9c6be322
Adi Dobrescu <adobrescu@insolitservices.ro>
2016-12-20 11:47:46 +0200

    Adds dummy empty constructor and ::map

--- lib/TTF/BaseMapping.class.php
+++ lib/TTF/BaseMapping.class.php
@@ -5,4 +5,16 @@ namespace TTF;
 
 class BaseMapping
 {
+	/*
+	 * As the class will work via a web service, it might be possible
+	 * to pass the input directly to the constructor and imediatly do the calculations
+	 * and store them inot a public var. 
+	 * I leave the constructor empty, All calculations are done by BaseMapping::map
+	 */
+	public function __construct()
+	{
+	}
+	public function map()
+	{
+	}
 }

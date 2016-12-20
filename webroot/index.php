ea2e853324193a411b5cb45e6af0ec7ecc6487c1
Adi Dobrescu <adobrescu@insolitservices.ro>
2016-12-20 14:15:23 +0200

    Creates empty files form the form

---
+++ webroot/index.php
@@ -0,0 +1,12 @@
+<!DOCTYPE html>
+<html>
+	<head>
+		<meta http-equiv="Content-Type" content="text/html; charset=utf-8">
+		<title>TTF App</title>
+		<link rel="stylesheet" type="text/css" href="lib/default.css">
+		<script type="text/javascript" src="lib/default.js"></script>
+	</head>
+	<body>
+		
+	</body>
+</html>
